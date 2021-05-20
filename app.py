5aeae73677ab176d2e614ba1c6011e470cf9b693
Abhinav <abhinavmunagala4@gmail.com>
2021-05-20 17:21:02 +0530

    applied flask

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-a = 2
-print("hello karthik")
+from flask import Flask,render_template,url_for
+
+app = Flask(__name__)
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
