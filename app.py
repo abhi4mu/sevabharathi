240e894734e64342d7e00313e1ba66698da10126
MakineniSuryaTej <64590688+MakineniSuryaTej@users.noreply.github.com>
2021-05-24 14:56:28 +0530

    teammembers

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/team')
+def team():
+    return render_template('team.html')
 if __name__=='__main__':
     app.run(debug=True)
