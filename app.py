dbc2ea96879be6d4f653640dd17d22db904d2e8b
Abhinav <abhinavmunagala4@gmail.com>
2021-05-27 19:44:22 +0530

    added links to activities pages

--- app.py
+++ app.py
@@ -6,15 +6,49 @@ app = Flask(__name__)
 def covid19():
     return render_template('covid19.html')
 
+@app.route('/education')
+def education():
+    return render_template('education.html')
+
+@app.route('/health')
+def health():
+    return render_template('health.html')
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/livelyhood')
+def livelyhood():
+    return render_template('livelyhood.html')    
+
+@app.route('/ourpartners')
+def ourpartners():
+    return render_template('ourpartners.html')
+
+@app.route('/relief')
+def relief():
+    return render_template('relief.html')
+
+@app.route('/samskara')
+def samskara():
+    return render_template('samskara.html')
+
+@app.route('/social')
+def social():
+    return render_template('social.html')
+
 @app.route('/team')
 def team():
     return render_template('team.html')
-@app.route('/ourparners')
-def ourpartners():
-    return render_template('ourpartners.html')
+
+@app.route('/tribal')
+def tribal():
+    return render_template('tribal.html')
+
+@app.route('/vanavasi')
+def vanavasi():
+    return render_template('vanavasi.html')
+
 if __name__=='__main__':
     app.run(debug=True)
